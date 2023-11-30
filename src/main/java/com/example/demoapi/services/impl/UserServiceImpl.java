dc6461e85f94ca2e75069892df4f2743e207f85f
nguyentieuat <thanhluan.kma@gmail.com>
2023-12-01 01:43:43 +0700

    update source

--- src/main/java/com/example/demoapi/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/demoapi/services/impl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import com.example.demoapi.models.Users;
 import com.example.demoapi.repositories.TransactionRepository;
 import com.example.demoapi.repositories.UserRepository;
 import com.example.demoapi.services.UserService;
-import com.example.demoapi.utils.Constant;
+import com.example.demoapi.utils.Utils;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -20,16 +20,8 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public void updateRankUser(Users user, String businessCode, float amount) {
 		Float sumTotalSpent = transactionRepository.sumTotalSpentByUsernameAndBusiness(user.getUsername(), businessCode);
-		
-		if (sumTotalSpent >= Constant.MOCK_GOLD && sumTotalSpent < Constant.MOCK_PLATINUM) {
-			user.setRank(1);
-		} else if (sumTotalSpent >= Constant.MOCK_PLATINUM && sumTotalSpent < Constant.MOCK_DIAMOND) {
-			user.setRank(2);
-		} else if (sumTotalSpent >=  Constant.MOCK_DIAMOND) {
-			user.setRank(3);
-		} else {
-			user.setRank(0);
-		}
+		int rank = Utils.getRank(sumTotalSpent);
+		user.setRank(rank);
 		userRepository.save(user);
 	}
 
